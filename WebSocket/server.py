3e6b7c4d162bcd7dcbd848ac1722d2b1ae986844
magdalucreteanu <magda.lucreteanu@gmail.com>
2022-01-07 12:40:01 +0100

    fix

--- WebSocket/server.py
+++ WebSocket/server.py
@@ -10,8 +10,6 @@ def client_left(client, server):
 
 # Called when a client sends a message
 def message_received(client, server, message):
-    if len(message) > 200:
-        message = message[:200]+'..'
     print("Client(%d) said: %s" % (client['id'], message))
     server.send_message_to_all(message)
 
