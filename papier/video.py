8c31adc14f5ccf8612e5ce2e2cbfd7699e536b22
magdalucreteanu <magda.lucreteanu@gmail.com>
2022-01-10 12:34:10 +0100

    Knöpfe in Video markieren

--- papier/video.py
+++ papier/video.py
@@ -19,6 +19,60 @@ h_black = int(0)
 s_black = int(0)
 v_black = int(0)
 
+# Koordinaten der Elemente innerhalb des originalen Bilders (mit den zwei Beispieltastaturen)
+# Das Bild befindet sich in media/Papier_Tastatur_Image.jpeg
+# Wir identifizieren este die Tastatur anhand des roten Randes.
+# Danach finden wir die einzelnen Elemente mathematisch anhand der Koordinaten.
+
+# Tastatur
+# Outer
+paper_outer_margin_upper_x = 171
+paper_outer_margin_upper_y = 1239
+paper_outer_margin_lower_x = 926
+paper_outer_margin_lower_y = 1607
+# Inner
+paper_inner_margin_upper_x = 192
+paper_inner_margin_upper_y = 1261
+paper_inner_margin_lower_x = 904
+paper_inner_margin_lower_y = 1586
+
+# Volume Minus
+paper_volume_minus_upper_x = 202
+paper_volume_minus_upper_y = 1108
+paper_volume_minus_w = 63
+paper_volume_minus_h = 63
+
+# Volume Plus
+paper_volume_plus_upper_x = 283
+paper_volume_plus_upper_y = 1108
+paper_volume_plus_w = 63
+paper_volume_plus_h = 63
+
+# Piano
+paper_piano_upper_x = 382
+paper_piano_upper_y = 1095
+paper_piano_w = 89
+paper_piano_h = 89
+
+# Synth
+paper_synth_upper_x = 504
+paper_synth_upper_y = 1095
+paper_synth_w = 89
+paper_synth_h = 89
+
+# Attack
+paper_attack_upper_x = 624
+paper_attack_upper_y = 1075
+paper_attack_w = 282
+paper_attack_h = 35
+
+# Release
+paper_release_upper_x = 624
+paper_release_upper_y = 1144
+paper_release_w = 282
+paper_release_h = 35
+
+
 # Callback Funktion für Slider - tut nichts
 def do_nothing():
     return
@@ -117,6 +171,51 @@ def findBiggestRegionsForColor(frame, h, s, v, threshold, numberRegions):
             cnt = contours[index]
     return mask, contours, biggestRegionIndex, cnt
 
+def getPixelSize(w):
+    return 1.0 * w / (paper_outer_margin_lower_x - paper_outer_margin_upper_x)
+
+def getVolumeMinusRegion(keyboard_x,keyboard_y,pixel_size):
+    volume_minus_x = keyboard_x + pixel_size * (paper_volume_minus_upper_x -  paper_outer_margin_upper_x)
+    volume_minus_y = keyboard_y + pixel_size * (paper_volume_minus_upper_y - paper_outer_margin_upper_y)
+    volume_minus_w = pixel_size * paper_volume_minus_w
+    volume_minus_h = pixel_size * paper_volume_minus_h
+    return int(volume_minus_x), int(volume_minus_y), int(volume_minus_w), int(volume_minus_h)
+
+def getVolumePlusRegion(keyboard_x,keyboard_y,pixel_size):
+    volume_plus_x = keyboard_x + pixel_size * (paper_volume_plus_upper_x -  paper_outer_margin_upper_x)
+    volume_plus_y = keyboard_y + pixel_size * (paper_volume_plus_upper_y - paper_outer_margin_upper_y)
+    volume_plus_w = pixel_size * paper_volume_plus_w
+    volume_plus_h = pixel_size * paper_volume_plus_h
+    return int(volume_plus_x), int(volume_plus_y), int(volume_plus_w), int(volume_plus_h)
+
+def getPianoRegion(keyboard_x,keyboard_y,pixel_size):
+    piano_x = keyboard_x + pixel_size * (paper_piano_upper_x -  paper_outer_margin_upper_x)
+    piano_y = keyboard_y + pixel_size * (paper_piano_upper_y - paper_outer_margin_upper_y)
+    piano_w = pixel_size * paper_piano_w
+    piano_h = pixel_size * paper_piano_h
+    return int(piano_x), int(piano_y), int(piano_w), int(piano_h)
+
+def getSynthRegion(keyboard_x,keyboard_y,pixel_size):
+    synth_x = keyboard_x + pixel_size * (paper_synth_upper_x -  paper_outer_margin_upper_x)
+    synth_y = keyboard_y + pixel_size * (paper_synth_upper_y - paper_outer_margin_upper_y)
+    synth_w = pixel_size * paper_synth_w
+    synth_h = pixel_size * paper_synth_h
+    return int(synth_x), int(synth_y), int(synth_w), int(synth_h)
+
+def getAttackRegion(keyboard_x,keyboard_y,pixel_size):
+    attack_x = keyboard_x + pixel_size * (paper_attack_upper_x -  paper_outer_margin_upper_x)
+    attack_y = keyboard_y + pixel_size * (paper_attack_upper_y - paper_outer_margin_upper_y)
+    attack_w = pixel_size * paper_attack_w
+    attack_h = pixel_size * paper_attack_h
+    return int(attack_x), int(attack_y), int(attack_w), int(attack_h)
+
+def getReleaseRegion(keyboard_x,keyboard_y,pixel_size):
+    release_x = keyboard_x + pixel_size * (paper_release_upper_x -  paper_outer_margin_upper_x)
+    release_y = keyboard_y + pixel_size * (paper_release_upper_y - paper_outer_margin_upper_y)
+    release_w = pixel_size * paper_release_w
+    release_h = pixel_size * paper_release_h
+    return int(release_x), int(release_y), int(release_w), int(release_h)
+
 
 # ------------------------- VIDEO  --------------------------------------------------------
 
@@ -151,8 +250,35 @@ while cap.isOpened():
     cv2.drawContours(mask, contours, biggestRegionIndex, (255,255,255), cv2.FILLED)
 
     # Keyboard zeichnen
-    x,y,w,h = cv2.boundingRect(cnt)
-    cv2.rectangle(frame, (x,y), (x+w, y+h), color=(0, 255, 0), thickness=2)
+    keyboard_x,keyboard_y,keyboard_w,keyboard_h = cv2.boundingRect(cnt)
+    cv2.rectangle(frame, (keyboard_x, keyboard_y), (keyboard_x + keyboard_w, keyboard_y + keyboard_h), color=(0, 255, 0), thickness=2)
+
+    # relative Pixelgrüße finden
+    pixel_size = getPixelSize(keyboard_w)
+
+    ## Volume Minus zeichnen
+    volume_minus_x, volume_minus_y, volume_minus_w, volume_minus_h = getVolumeMinusRegion(keyboard_x,keyboard_y,pixel_size)
+    cv2.rectangle(frame, (volume_minus_x, volume_minus_y), (volume_minus_x + volume_minus_w, volume_minus_y + volume_minus_h), color=(0, 255, 0), thickness=2)
+ 
+    ## Volume Plus zeichnen
+    volume_plus_x, volume_plus_y, volume_plus_w, volume_plus_h = getVolumePlusRegion(keyboard_x,keyboard_y,pixel_size)
+    cv2.rectangle(frame, (volume_plus_x, volume_plus_y), (volume_plus_x + volume_plus_w, volume_plus_y + volume_plus_h), color=(0, 255, 0), thickness=2)
+
+    ## Piano zeichnen
+    piano_x, piano_y, piano_w, piano_h = getPianoRegion(keyboard_x,keyboard_y,pixel_size)
+    cv2.rectangle(frame, (piano_x, piano_y), (piano_x + piano_w, piano_y + piano_h), color=(0, 255, 0), thickness=2)
+
+    ## Synth zeichnen
+    synth_x, synth_y, synth_w, synth_h = getSynthRegion(keyboard_x,keyboard_y,pixel_size)
+    cv2.rectangle(frame, (synth_x, synth_y), (synth_x + synth_w, synth_y + synth_h), color=(0, 255, 0), thickness=2)
+
+    ## Attack zeichnen
+    attack_x, attack_y, attack_w, attack_h = getAttackRegion(keyboard_x,keyboard_y,pixel_size)
+    cv2.rectangle(frame, (attack_x, attack_y), (attack_x + attack_w, attack_y + attack_h), color=(0, 255, 0), thickness=2)
+
+    ## Release zeichnen
+    release_x, release_y, release_w, release_h = getReleaseRegion(keyboard_x,keyboard_y,pixel_size)
+    cv2.rectangle(frame, (release_x, release_y), (release_x + release_w, release_y + release_h), color=(0, 255, 0), thickness=2)
 
     # Kombiniertes Ergebnis anzeigen
     cv2.imshow('Tastatur', mask)
